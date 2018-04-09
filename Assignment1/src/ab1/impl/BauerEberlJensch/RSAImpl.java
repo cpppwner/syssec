28d5996d2c2c2afc2d6b584aec5fe446e62df2fa
Stefan Eberl <cpppwner@gmail.com>
2018-04-09 21:45:05 +0200

    Add initial sources
    Add sources downloaded from Moodle.

---
+++ Assignment1/src/ab1/impl/BauerEberlJensch/RSAImpl.java
@@ -0,0 +1,52 @@
+package ab1.impl.BauerEberlJensch;
+
+import ab1.RSA;
+
+public class RSAImpl implements RSA {
+
+	@Override
+	public void init(int n) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public PublicKey getPublicKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public PrivateKey getPrivateKey() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] encrypt(byte[] data, boolean activateOAEP) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] decrypt(byte[] data) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] sign(byte[] message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] verify(byte[] message, byte[] signature) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+}
